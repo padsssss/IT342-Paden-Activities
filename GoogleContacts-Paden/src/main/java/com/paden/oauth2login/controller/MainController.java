5a4a60eafd17c1866b9c3afcc228fd398a9a5dac
LAB23_USER ON WS38 <L23X14W38@citulab.net>
2025-03-07 13:55:19 +0800

    Commit google contacts integration

---
+++ GoogleContacts-Paden/src/main/java/com/paden/oauth2login/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.paden.oauth2login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+} 
